09e33b2ad701363d2fab8751ed416e0eff06a5fa
Mathieu-R <mathieu0709@gmail.com>
2017-03-01 12:56:29 +0100

    add docker-compose to init.sh

--- VPS-SSH/init.sh
+++ VPS-SSH/init.sh
@@ -4,3 +4,10 @@ echo "mise à jours des repos..."
 sudo apt-get update
 echo "installation de vim..."
 sudo apt-get install vim 
+echo "installation de docker-compose"
+sudo rm /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-`uname -s`-`uname -m` > docker-compose
+chmod +x docker-compose
+sudo mv docker-compose /usr/local/bin
+echo "vérification de la version de docker-compose"
+docker-compose --version
