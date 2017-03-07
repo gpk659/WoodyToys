c5278c494574d34ecda6d75327a3a6446fed1554
Mathieu-R <mathieu0709@gmail.com>
2017-03-07 16:35:36 +0100

    db connection and query (marketplace)

--- websites/b2b.wt4.ephec-ti.be/server.js
+++ websites/b2b.wt4.ephec-ti.be/server.js
@@ -3,9 +3,19 @@ const express = require('express');
 const serveStatic = require('serve-static'); // static files
 const compression = require('compression'); // gzip
 const mongoose = require('mongoose');
-
 const app = express();
 const prod = process.env.NODE_ENV == 'production';
+
+mongoose.promise = global.promise;
+mongoose.createConnection('mongodb://localhost/marketplace');
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    category: String,
+    cost: Integer
+});
+
+const product = mongoose.model('product', productSchema);
 
 app.use(compression());
 
@@ -17,6 +27,10 @@ app.use(serveStatic('static', {
 app.get('/', (req, res) => {
     res.send('index.html');
 })
+
+app.get('/products', async (req, res) => {
+    //const products = await product.findAll();
+});
 
 http.createServer(app).listen(8080, _ => {
     console.log('Listening on 8080');
