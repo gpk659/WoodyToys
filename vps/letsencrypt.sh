d451e82dabb4f16a6250c19bb1eb219d2166490e
Mathieu-R <mathieu0709@gmail.com>
2017-03-11 21:43:40 +0100

    moving to https

--- vps/letsencrypt.sh
+++ vps/letsencrypt.sh
@@ -1,8 +1,6 @@
-#!/bin/bash 
+#!/bin/bash
 
 echo "mise à jours des repos..."
 sudo apt-get update
-echo "clonage du repo let's encrypt..."
-# dans /opt/letsencrypt, historique tronqué
-sudo git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt --depth=1
-cd /opt/letsencrypt
+echo "téléchargement de letsencrypt"
+sudo apt install letsencrypt
