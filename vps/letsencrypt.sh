c54b424be8abecc3898635ad5bf1e7f1c9c5250d
root <root@vps375420.ovh.net>
2017-05-06 17:47:24 +0200

    Config of second vps

--- vps/letsencrypt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "mise à jours des repos..."
-sudo apt-get update
-echo "téléchargement de letsencrypt"
-sudo apt install letsencrypt
--- vps/nmap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "mise à jours des repos..."
-sudo apt-get update
-echo "installation de nmap..."
-sudo apt-get install nmap
-echo "ports ouverts sur le VPS..."
-sudo nmap -sS -sU localhost
